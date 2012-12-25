458fa6930018213aae12afe8766fa39f56d6423a
Daniel Olshansky <olshansky.daniel@gmail.com>
2012-12-24 19:35:46 -0500

    Initial Commit

---
+++ ProjectOak/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  ProjectOak
+//
+//  Created by Daniel Olshansky on 2012-12-24.
+//  Copyright (c) 2012 Daniel Olshansky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ ProjectOak/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ProjectOak
+//
+//  Created by Daniel Olshansky on 2012-12-24.
+//  Copyright (c) 2012 Daniel Olshansky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
